cb036b61aa85a50fcbd9b205b1e3ecc09742eb85
Carlos Rendon <crendon@gmail.com>
2016-10-29 15:34:33 -0700

    Creates csv output

--- website/server.py
+++ website/server.py
@@ -1,10 +1,11 @@
 import os
+import subprocess
 from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['UPLOAD_FOLDER'] = 'C:/Users/Carlos/Documents/GitHub/turbo-carnival/website/uploads'
 # 2 megs max
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 
@@ -36,7 +37,22 @@ def upload_file():
     
 @app.route("/view_replay/<filename>")
 def view_replay(filename):
-    return render_template("visualize.html", filename)
+    in_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    json_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename + ".json")
+    csv_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename + ".csv")
+    
+    octane_in = open(in_filename, "r")
+    octane_out = open(json_filename, "w")
+
+    
+    res = subprocess.call(['octane'], stdin=octane_in, stdout=octane_out)
+    if res == 0:
+        csv_in = open(json_filename, "r")
+        csv_out = open(csv_filename, "w")
+        res = subprocess.call(['python', '../rocket_league_replay_decode.py'], stdin=csv_in, stdout=csv_out)
+        if res == 0:
+            return render_template("visualize.html", filename=csv_filename)
+    return "error processing"
 
 if __name__ == "__main__":
     app.run()

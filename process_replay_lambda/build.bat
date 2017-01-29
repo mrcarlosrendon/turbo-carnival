f943ccc83718789ab3733eba02c89510346e2b7f
Carlos Rendon <crendon@gmail.com>
2017-01-29 13:11:23 -0800

    zip ALL of the files

--- process_replay_lambda/build.bat
+++ process_replay_lambda/build.bat
@@ -4,7 +4,7 @@ move octane build
 python -m pip install -t build -r requirements.txt
 del build.zip
 cd build
-zip build.zip *
+zip -r build.zip *
 move build.zip ..
 cd ..
 aws lambda update-function-code --function-name process-replay --zip-file fileb://build.zip --publish
